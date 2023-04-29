e48d775c973b93ee5a5447170cec43cb28acc3d4
root <maxfred304@gmail.com>
2023-04-29 09:13:27 +0000

    Updated API files and Tasks

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,37 @@
-from flask import jsonify
+#!/usr/bin/python3
+"""
+Flask route that returns json status response
+"""
 from api.v1.views import app_views
+from flask import jsonify, request
+from models import storage
 
-# Create a route on the app_views Blueprint instance
+
 @app_views.route('/status', methods=['GET'])
-def get_status():
-    # Return a JSON response with a "status" key and "OK" value
-    return jsonify({"status": "OK"})
+def status():
+    """
+    function for status route that returns the status
+    """
+    if request.method == 'GET':
+        resp = {"status": "OK"}
+        return jsonify(resp)
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)
